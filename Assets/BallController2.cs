a52aed169ed215495c6820a04350b8935e02fda1
Kinga Stachelek <kstachelek@edu.cdv.pl>
2023-06-04 19:58:50 +0200

    Almost Finished Pong Game

---
+++ Assets/BallController2.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController2 : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float speed = 5f;
+    public Vector3 vel;
+    public bool isPlaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        ResetAndSendBallInRandomDirection();
+        ResetBall();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isPlaying == false)
+        {
+            ResetAndSendBallInRandomDirection();
+        }
+    }
+
+    private void ResetAndSendBallInRandomDirection()
+    {
+        ResetBall();
+        rb2D.velocity = GenerateRandomVelocity(true) * speed;
+        vel = rb2D.velocity;
+        isPlaying = true;
+    }
+
+    private void ResetBall()
+    {
+        rb2D.velocity = Vector3.zero;
+        transform.position = Vector3.zero;
+        isPlaying = false;
+    }
+          
+    
+
+    private Vector3 GenerateRandomVelocity(bool shouldReturnNormalized)
+    {
+        Vector3 velocity = new Vector3();
+        bool shouldGoRight = Random.Range(1, 100) > 50;
+        velocity.x = shouldGoRight ? Random.Range(-0.8f, -0.3f): Random.Range(0.8f, 0.3f);
+        velocity.y = shouldGoRight ? Random.Range(-0.8f, -0.3f): Random.Range(0.8f, 0.3f);
+
+        if (shouldReturnNormalized)
+        {
+            return velocity.normalized;
+        }
+
+        return velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 newVelocity = vel;
+        newVelocity += new Vector3(Random.Range(-.5f, .5f), Random.Range(-.5f, .5f));
+        rb2D.velocity = Vector3.Reflect(newVelocity.normalized * speed, collision.contacts[0].normal);
+        vel = rb2D.velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (transform.position.x > 0)
+            print("Left Player +1");
+
+        if (transform.position.x < 0)
+            print("Right Player +1");
+
+        ResetAndSendBallInRandomDirection();
+        ResetBall();
+    }
+
+
+}
